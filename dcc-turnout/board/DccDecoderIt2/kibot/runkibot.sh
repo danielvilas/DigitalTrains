73da8425d2e08a27c5621b3c7030b5b16b0012ae
Daniel Vilas <daniel.vilas.perulan@everis.com>
2021-09-07 14:58:47 +0200

    Fab iteration

--- dcc-turnout/board/DccDecoderIt2/kibot/runkibot.sh
+++ dcc-turnout/board/DccDecoderIt2/kibot/runkibot.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 rm -rf doc
+
+KIBOT_DIR=$(git rev-parse --show-toplevel)
+KIBOT_DIR=$KIBOT_DIR/kibot
+
 kibot  -e DccDecoder.sch -b DccDecoder.kicad_pcb -d doc -c DccDecoder.kibot.yaml
+kibot  -e DccDecoder.sch -b DccDecoder.kicad_pcb -d doc -c $KIBOT_DIR/05_fab_pcba.kibot.yaml
