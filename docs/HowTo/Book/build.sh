13e294c608139c71fb1d35a2ce7b4604683bb095
Daniel Vilas Perulan <daniel.vilas@gmail.com>
2022-01-07 20:33:21 +0100

    Moved Documentation

--- docs/HowTo/Book/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#rm main.aux  main.bcf  main.blg  main.dvi  main.idx  main.ilg  main.ind  main.log  main.out.ps  main.pdf  main.ptc  main.run.xml  main.toc
-
-pdflatex main
-makeindex main
-biber main
-pdflatex main
